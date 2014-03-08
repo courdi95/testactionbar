30c132e00a9dffc4d6b6c37522cc7542def5481c
courdi95 <courdi95@gmail.com>
2014-03-08 23:40:56 +0100

    menu

--- src/com/courdi95/testactionbar/MainActivity.java
+++ src/com/courdi95/testactionbar/MainActivity.java
@@ -2,6 +2,7 @@ package com.courdi95.testactionbar;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
 
 
 public class MainActivity extends Activity
@@ -13,4 +14,14 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+
+@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.actionbar, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 }
+
